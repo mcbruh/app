c90a028e1a25aac874fb23a6cd1ad7334f0497fa
mcbruh <michael.chen.1120@gmail.com>
2017-05-24 14:34:49 -0400

    added index views for recipes and fixed routing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,14 @@
 class RecipesController < ApplicationController
-	before_filter :authenticate_user!, except: [:index, :show]
+	before_action :authenticate_user!, except: [:index, :show]
+
+	def index
+		@user = current_user if user_signed_in?
+		@recipes = Recipe.all	
+	end
+
+	private
+
+	def recipe_params
+		params.require(:recipe).permit(:name,:description, :instructions, :user_id, :recipe_ingredients_attributes => [:id, :ingredient_id, :recipe_id, :quantity] )
+	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def welcome
     if user_signed_in?
     	@user = current_user
-      	redirect_to user_path(@user)
+      	redirect_to recipes_path
     else
       	redirect_to new_user_session_path
     end
